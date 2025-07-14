6768d506affc0260e90363daf679c733a1d890a5
Paulius Budreika <paulius.budreika0@gmail.com>
2025-07-14 20:45:45 +0300

    Added new plugins
    New plugins have been added:
    - LazyGit
    - WhichKey
    Also added desciptions for a few leader options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,8 +15,8 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {desc = "Live grep"})
       vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
       vim.keymap.set("n", "<C-m>", ":Telescope treesitter<CR>", {})
       require("telescope").load_extension("ui-select")
